cef82a2dec7b7e581717532e298f5420d964698b
nyk-o-brown <odoyobrown3@gmail.com>
2025-10-17 02:34:44 +0300

    msg:stuck at a url err,fixed migrate issue

--- property_manager_backend/apps/users/urls.py
+++ property_manager_backend/apps/users/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include  # ✅ include is needed
 
 urlpatterns = [
-    # path('', include(...)),
+    path('users/', include('users.urls')),         # 🔗 routes to users app
+    path('properties/', include('properties.urls'))  # 🔗 routes to properties app
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
