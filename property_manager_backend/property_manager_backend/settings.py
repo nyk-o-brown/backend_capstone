4634308d43e4a0f88207a0c502d76809d7b203bd
nyk-o-brown <odoyobrown3@gmail.com>
2025-10-19 06:28:16 +0300

    msg:api config

--- property_manager_backend/property_manager_backend/settings.py
+++ property_manager_backend/property_manager_backend/settings.py
@@ -60,7 +60,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware',] + MIDDLEWARE
+#MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware',] + MIDDLEWARE
+
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
 
 ROOT_URLCONF = 'property_manager_backend.urls'
 
@@ -133,6 +138,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+#########################################
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
